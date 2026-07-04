87404656bd675d1343ae2a2e15ff14e184e7a62a
Sinchu <sinchukm312@gmail.com>
2026-07-04 15:55:36 +0530

    fix acr name unique

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "myacr19"
+  name                = "myacr19devops123"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
