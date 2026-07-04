9e909320404763c0cac29707784f969311d0d93b
Sinchu <sinchukm312@gmail.com>
2026-07-04 12:04:37 +0530

    add workflow

--- main.tf
+++ main.tf
@@ -1,4 +1,16 @@
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
+  name     = "my-rg"
+  location = "centralindia"
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "myacr19"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
 }
