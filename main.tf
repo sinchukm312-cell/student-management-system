74675d340dbfdb2af11f972656d149c0fd906a11
Sinchu <sinchukm312@gmail.com>
2026-07-05 18:50:21 +0530

    trigger pipeline

--- main.tf
+++ main.tf
@@ -1,11 +1,10 @@
-# ✅ Existing Resource Group
+# Existing Resource Group
 data "azurerm_resource_group" "rg" {
   name = "student-devops-rg"
 }
 
-# ✅ Existing ACR
+# Existing ACR
 data "azurerm_container_registry" "acr" {
   name                = "myacr19"
   resource_group_name = "my-rg"
 }
-
--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,7 @@
-output "resource_group_name" {
-  value = data.azurerm_resource_group.rg.name
+output "vm_public_ip" {
+  value = "20.196.138.196"
 }
 
 output "acr_login_server" {
   value = data.azurerm_container_registry.acr.login_server
-}
-
-output "vm_public_ip" {
-  value = azurerm_public_ip.publicip.ip_address
 }
