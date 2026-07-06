3634965fbe5d93d142a70d5f640a3aa178d56cdb
Sinchu <sinchukm312@gmail.com>
2026-07-06 10:32:54 +0530

    fixed terraform folder (removed submodule)

---
+++ terraform/main.tf
@@ -0,0 +1,10 @@
+# Existing Resource Group
+data "azurerm_resource_group" "rg" {
+  name = "student-devops-rg"
+}
+
+# Existing ACR
+data "azurerm_container_registry" "acr" {
+  name                = "myacr19"
+  resource_group_name = "my-rg"
+}
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,2 @@
+resource_group_name = "student-devops-rg"
+location            = "westus2"
